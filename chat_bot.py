cf2b11f1934c9aceb956852b13f0855ead5a2f49
David Shaw <david.shaw2014@gmail.com>
2017-03-20 03:40:49 -0400

    Changed IO to file input/output

--- chat_bot.py
+++ chat_bot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2.7
 
 import sys
+import os
+import time
 import random
 
 
@@ -9,7 +11,7 @@ greetings = ("hi", "hello", "how are you,")
 
 responses = ["well hello there", "hey", "wassup?"]
 
-input = raw_input("Please enter a message: ")
+# input = raw_input("Please enter a message: ")
 
 def respond_to_greeting(input):
 	 
@@ -19,6 +21,37 @@ def respond_to_greeting(input):
 	return "*smiles and nods*"			
 			
 
-output = respond_to_greeting(input)
-print output
+# Open input file for dialogue
+fin = open("chat_bot_input.txt")
+
+# Create output file for dialogue or append if it already exists
+if( not os.path.exists("chat_bot_output.txt") ):
+	fout = open("chat_bot_output.txt", 'w')
+	fout.write('\n')
+else:
+	fout = open("chat_bot_output.txt", 'a')
+	fout.write('\n')
+
+# Begin log with date of conversation
+gmtime = time.gmtime()
+fout.write(str(gmtime.tm_mday) + '-' + str(gmtime.tm_mon) + '-' + str(gmtime.tm_year) + '\n')
+
+# Engage in dialogue
+for input in fin:
+
+	# Write user input to log
+	fout.write("user:\t\t" + input.strip() + '\n')
+
+	# Determine chat bot response and write it to log
+	output = respond_to_greeting(input)
+	fout.write("chat_bot:\t" + output.strip() + '\n')
+
+# print output
+
+# Leave some space for the next entry
+fout.write('\n')
+
+# Close files
+fin.close()
+fout.close()
 
